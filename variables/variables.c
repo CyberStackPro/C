4e6586f57667ea58210a381bfc648dba76631408
CyberStackPro <yeabsragebriel@gmail.com>
2025-05-15 23:55:41 +0300

    Add some prototype

--- variables/variables.c
+++ variables/variables.c
@@ -1,27 +1,92 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/utsname.h>
 
-//
-int main()
+int variables(){
+
+    int age;
+    float salary;
+}
+int add(int a, int b);
+
+void info(const struct utsname *);
+
+// int main()
+// {
+//    int numbers[5] ={10,20,30,40,50};
+
+//    char name[20] = "Alice";
+
+//   printf("First number: %d\n", numbers[0]);
+//   printf("Third  number: %d\n", numbers[2]);
+
+
+//   printf("Name: %s\n", name);
+
+//   printf("Length of name: %lu\n", strlen(name));
+
+//   char anotherName[20];
+//   strcpy(anotherName, name);
+
+//   printf("Copied name: %s\n", anotherName);
+//   return 0;
+// }
+
+
+
+int main(){
+    pid_t pid = getpid();
+    printf("My PID is %d\n", pid);
+
+
+    struct utsname sys_info;
+    if(uname(&sys_info) == 0){
+        // printf("System: %s %s %s\n",
+        //              info.sysname,
+        //              info.release,
+        //              info.machine
+        // );
+        info(&sys_info);
+
+    }else {
+          perror("uname");
+    }
+
+ return 0;
+}
+
+void info(const struct utsname *data) {
+    printf("System name: %s\n", data->sysname);
+    printf("Node name: %s\n", data->nodename);
+    printf("Release: %s\n", data->release);
+    printf("Version: %s\n", data->version);
+    printf("Machine: %s\n", data->machine);
+}
+
+
+
+int add(int a, int b)
 {
-    // variables are containers for storing data
-
-    int x;       // declaration
-    x = 123;     // iitialazation
-    int y = 321; // dec + init
-
-    int age = 21; // integer
-
-    float gpa = 2.05; // float
-
-    char grade = 'C';     // string char
-    char name[] = "Yeab"; // array of characters
-
-    // format specifiers like %d %f %c and more ...
-    printf("Hello My Name is %s\n", name);
-    printf("I'm im %d years old\n", age);
-    printf("Your Average grade is %c\n", grade);
-    printf("Your Gpa is %f\n", gpa);
-
-    /* code */
-    return 0;
+    return a + b;
+}
+int subtract(int a, int b)
+{
+    return a - b;
+}
+int multiply(int a, int b)
+{
+    return a * b;
+}
+int divide(int a, int b)
+{
+    if (b != 0)
+    {
+        return a / b;
+    }
+    else
+    {
+        printf("Error: Division by zero is not allowed.\n");
+        return 0; // Return 0 or handle the error as needed
+    }
 }
